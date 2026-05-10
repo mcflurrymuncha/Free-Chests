2d4389652be95765fcc9c9aeb72696b00f238275
mcflurry <vexithereal@gmail.com>
2026-05-10 16:44:27 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/GameStatsManager.hpp>
+#include <Geode/modify/GJRewardObject.hpp>
 
 using namespace geode::prelude;
 
+// Bypass currency checks in the Stats Manager
 class $modify(GameStatsManager) {
-    // This bypasses the "Do you have enough?" check
     bool hasStatEnough(int statID, int amount) {
         if (Mod::get()->getSettingValue<bool>("enabled")) {
             return true;
@@ -12,14 +13,21 @@ class $modify(GameStatsManager) {
         return GameStatsManager::hasStatEnough(statID, amount);
     }
 
-    // This makes the game think you have 999 of everything (Keys, Diamonds, etc.)
     int getStat(int statID) {
         if (Mod::get()->getSettingValue<bool>("enabled")) {
-            // Stat 14 = Demon Keys, Stat 29 = Diamonds
-            if (statID == 14 || statID == 29) {
-                return 9999;
-            }
+            // 14: Demon Keys, 29: Diamonds
+            if (statID == 14 || statID == 29) return 999;
         }
         return GameStatsManager::getStat(statID);
+    }
+};
+
+// Force the chest objects to report themselves as "unlocked"
+class $modify(GJRewardObject) {
+    bool canUnlockWithKey() {
+        if (Mod::get()->getSettingValue<bool>("enabled")) {
+            return true;
+        }
+        return GJRewardObject::canUnlockWithKey();
     }
 };
